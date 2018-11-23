6b0cd1863192a145dcc4214e6938b4f4f1257e52
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-23 15:34:57 +0530

    init config

--- src/App.js
+++ src/App.js
@@ -8,18 +8,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <main>
+          <section>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </section>
+          <section>App Content</section>
+        </main>
       </div>
     )
   }

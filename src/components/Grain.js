1549804e4197c3dc4608fc1135be8b658dc2d7ef
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-23 17:27:36 +0530

    grains

--- src/components/Grain.js
+++ src/components/Grain.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import cn from 'classnames'
+import cn from 'classname'
+import './Grain.css'
 
 class Grain extends Component {
   static propTypes = {

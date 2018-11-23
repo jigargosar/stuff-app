291933df6157328a857521a7ecfbd452a0b4a532
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-23 15:56:11 +0530

    grains

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
               type="text"
               name="title"
               id="grain-title"
+              onKeyDown={e => storageSet('grain-title-input', e.target.value)}
             />
             {grains.map((grain, idx) => (
               <Grain key={idx} title={grain.title} />

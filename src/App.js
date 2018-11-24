a5d898578542a4d4693cac4ba39b7fa8accb673a
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-24 18:12:00 +0530

    try new react hooks api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { compose, isNil, mergeDeepRight } from 'ramda'
@@ -24,22 +24,36 @@ export function cacheAppState(state) {
   storageSet(appStateStorageKey(), state)
 }
 
-// APP
-class App extends Component {
-  state = loadAppState()
+function Example({ count, setCount }) {
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(ct => ct + 1)}>Click me</button>
+    </div>
+  )
+}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <main>
-          <p>HelloWorld</p>
-        </main>
-      </div>
-    )
-  }
+// APP
+
+function App() {
+  // const [state, setState] = useState(() => {
+  //   console.log('App use state Called')
+  //   return loadAppState()
+  // })
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main>
+        <p>HelloWorld</p>
+        <Example count={count} setCount={setCount} />
+      </main>
+    </div>
+  )
 }
 
 export default App

4b9dfe700618977325de5c06d5fdc91bbc4e9c6d
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-26 17:34:35 +0530

    hygen templates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,20 +2,15 @@ import React from 'react'
 
 // @formatter:off
 /* eslint-disable no-unused-vars */
-
 // noinspection all
 import PropTypes from 'prop-types'
 // noinspection all
 import R from 'ramda'
-
 /* eslint-disable no-unused-vars */
 // @formatter:on
-const Input = props => {
-  return (
-    <div>
 
-    </div>
-  )
+const Input = props => {
+  return <div />
 }
 
 Input.propTypes = {}
@@ -23,4 +18,3 @@ Input.propTypes = {}
 Input.defaultProps = {}
 
 export default Input
-

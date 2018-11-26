173470bcc1914f18c86bd40d5513ead8bfd37c1e
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-26 16:08:46 +0530

    hygen templates

---
+++ _templates/c/new/default.md
@@ -0,0 +1,22 @@
+---
+to: src/components/<%=name%>.jsx
+---
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import R from 'ramda'
+
+const <%=name%> = props => {
+return (
+<div>
+
+    </div>
+
+)
+}
+
+<%=name%>.propTypes = {}
+
+<%=name%>.defaultProps = {}
+
+export default <%=name%>
---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+// noinspection all
+import PropTypes from 'prop-types'
+// noinspection all
+import R from 'ramda'
+
+/* eslint-disable no-unused-vars */
+
+/* eslint-disable no-unused-vars */
+
+const Input = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+Input.propTypes = {}
+
+Input.defaultProps = {}
+
+export default Input
+

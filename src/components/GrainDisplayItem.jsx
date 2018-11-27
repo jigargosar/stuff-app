90a07400498c2bc12388c0cf76fc01400168b23c
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-27 17:36:20 +0530

    extract components

--- src/components/GrainDisplayItem.jsx
+++ src/components/GrainDisplayItem.jsx
@@ -11,40 +11,53 @@ import * as R from 'ramda'
 
 import React from 'react'
 import { hotKeys } from '../hotKeys'
-import { deleteGrain, getGrainDomId, grainSetDoneProp, startEditingSelectedGrainTrigger } from '../state'
+import {
+  deleteGrain,
+  getGrainDomId,
+  grainSetDoneProp,
+  startEditingSelectedGrainTrigger,
+} from '../state'
 import { Box } from 'rebass'
 import CheckBox from './CheckBox'
 import { FRowCY } from './styled'
 
-const GrainDisplayItem = ({ isSelected, grain, immerState, ...otherProps }) => {
-  return <FRowCY
-    py={2}
-    className={`bb b--light-gray ${isSelected ? 'bg-light-blue' : ''}`}
-    onKeyDown={hotKeys([
-      'Enter',
-      ev => {
-        if (ev.target.id === getGrainDomId(grain)) {
-          startEditingSelectedGrainTrigger(immerState)
-        }
-      },
-    ])}
-    {...otherProps}
-  >
-    <Box p={2}>
-      <CheckBox
-        value={grain.done}
-        onChange={bool => grainSetDoneProp(bool, grain, immerState)}
-      />
-    </Box>
-    <Box className="flex-auto">{grain.title}</Box>
-    <button onClick={() => deleteGrain(grain, immerState)}>X</button>
-  </FRowCY>
+const GrainDisplayItem = ({
+  isSelected,
+  grain,
+  immerState,
+  setState,
+  ...otherProps
+}) => {
+  return (
+    <FRowCY
+      py={2}
+      className={`bb b--light-gray ${isSelected ? 'bg-light-blue' : ''}`}
+      onKeyDown={hotKeys([
+        'Enter',
+        ev => {
+          if (ev.target.id === getGrainDomId(grain)) {
+            startEditingSelectedGrainTrigger(immerState)
+          }
+        },
+      ])}
+      {...otherProps}
+    >
+      <Box p={2}>
+        <CheckBox
+          value={grain.done}
+          onChange={bool => grainSetDoneProp(bool, grain, immerState)}
+        />
+      </Box>
+      <Box className="flex-auto">{grain.title}</Box>
+      <button onClick={() => setState(deleteGrain(grain))}>X</button>
+    </FRowCY>
+  )
 }
 
 GrainDisplayItem.propTypes = {
   grain: PropTypes.object.isRequired,
   immerState: PropTypes.func.isRequired,
-  isSelected: PropTypes.bool.isRequired
+  isSelected: PropTypes.bool.isRequired,
 }
 
 GrainDisplayItem.defaultProps = {}
